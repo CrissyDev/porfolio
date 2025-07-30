28baa59d8f407d94e9ceb06867bb92c64fe3c6f0
Crissy <christalriziki@gmail.com>
2025-07-31 01:41:55 +0300

    additions

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,7 +11,38 @@ import { Component } from '@angular/core';
 export class AboutComponent {
   activeTab: string = 'skills';
 
-  setActiveTab(tab: string) {
+  experiences = [
+    {
+      title: 'Frontend Developer',
+      company: 'Phonix Dimas.',
+      duration: 'May 2025-July 2025',
+      description: `3 Month Attachment, building Software Using Angular, Use of API's`
+    },
+    {
+      title: 'Master Trainer',
+      company: 'Sote HUB',
+      duration: 'Dec 2024-July 2025',
+      description: `Trained Msmes on how to Scale their Businesses online and bring profits, buld Websites
+      using plartforms like Odoo, WordPress`
+    }
+  ];
+
+  educations = [
+    {
+      degree: 'Bachelor of Science in Information Technology',
+      institution: 'Technical University of Mombasa',
+      duration: '2022 - Present',
+      description: `Focused on software development, algorithms, and web technologies. Graduated with honors and built several academic projects.`
+    },
+    {
+      degree: 'Kenya Certificate of Secondary Education (KCSE)',
+      institution: 'Mudavadi Girls High School',
+      duration: '2014 - 2018',
+      description: `Completed secondary education with a strong emphasis on sciences and mathematics.`
+    }
+  ];
+
+  setActiveTab(tab: string): void {
     this.activeTab = tab;
   }
 }
